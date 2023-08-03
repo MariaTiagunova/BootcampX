b4d75af0e469f4d73ee43b0468ad320384f907de
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-08-03 15:41:46 -0400

    Modify to protect from SQL injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-const cohort = process.argv[2];
+const cohortName = process.argv[2];
+const values = [`%${cohortName}%`];
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
